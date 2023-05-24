c7c163ff3f2536cae35bca08d536780f5d43d93c
Julio <julio.mech@hotmail.com>
2023-05-24 09:38:06 -0300

    test http get with json

--- front-end-angular/src/app/courses/services/courses.service.ts
+++ front-end-angular/src/app/courses/services/courses.service.ts
@@ -1,16 +1,19 @@
+import { Course } from './../model/course';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
+  private readonly API = '/assets/courses.json';
   constructor(private http: HttpClient) { }
 
   list() {
-    return [
-      { _id: '1', name: 'Angular', category: 'Front-end' }
-    ];
+    return this.http.get<Course[]>(this.API).pipe(
+      tap(courses => console.log(courses))
+    );
   }
 }
